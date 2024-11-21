cf7d53fa4c1daea8528ed4b3203fa574cc28efdd
Jerry Xie <jerryxie@microsoft.com>
2024-11-20 18:57:11 -0600

    examples: run cargo +nightly fmt

--- examples/rt685s-evk/src/lib.rs
+++ examples/rt685s-evk/src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 
-use defmt_rtt as _;
 use mimxrt600_fcb::FlexSPIFlashConfigurationBlock;
-use panic_probe as _;
+use {defmt_rtt as _, panic_probe as _};
 
 #[link_section = ".otfad"]
 #[used]
