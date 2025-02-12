fbb48fcbb47b6cfdaaa9ccbd426748b86f28b394
Robert Zieba <robertzieba@microsoft.com>
2025-02-11 18:19:09 -0700

    embedded-service/power/policy: Introduce sealed trait for type states

--- embedded-service/src/power/policy/action/mod.rs
+++ embedded-service/src/power/policy/action/mod.rs
@@ -8,14 +8,18 @@ mod policy;
 pub use device::*;
 pub use policy::*;
 
+trait Sealed {}
+
 /// Trait to provide the kind of a state type
-pub trait Kind {
+#[allow(private_bounds)]
+pub trait Kind: Sealed {
     /// Return the kind of a state type
     fn kind() -> StateKind;
 }
 
 /// State type for a detached device
 pub struct Detached;
+impl Sealed for Detached {}
 impl Kind for Detached {
     fn kind() -> StateKind {
         StateKind::Detached
@@ -24,6 +28,7 @@ impl Kind for Detached {
 
 /// State type for an attached device
 pub struct Idle;
+impl Sealed for Idle {}
 impl Kind for Idle {
     fn kind() -> StateKind {
         StateKind::Idle
@@ -32,6 +37,7 @@ impl Kind for Idle {
 
 /// State type for a device that is providing power
 pub struct ConnectedProvider;
+impl Sealed for ConnectedProvider {}
 impl Kind for ConnectedProvider {
     fn kind() -> StateKind {
         StateKind::ConnectedProvider
@@ -40,6 +46,7 @@ impl Kind for ConnectedProvider {
 
 /// State type for a device that is consuming power
 pub struct ConnectedConsumer;
+impl Sealed for ConnectedConsumer {}
 impl Kind for ConnectedConsumer {
     fn kind() -> StateKind {
         StateKind::ConnectedConsumer
