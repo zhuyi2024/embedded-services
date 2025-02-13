74946d72da4dd2a815b488777f9ea05c40d4872b
Robert Zieba <robertzieba@microsoft.com>
2025-02-13 10:20:37 -0700

    embedded-service: refactor logging macros
    Split up logging macros implementations along feature lines. Using these
    macros as is in dependent dependent crates can leak the `defmt` and
    `log` features resulting in warnings if it only supports one of the
    features. Change checks to support these mutually exclusive features.

--- embedded-service/src/fmt.rs
+++ embedded-service/src/fmt.rs
@@ -1,81 +1,179 @@
 //! Logging macro implementations and other formating functions
 
-/// Logs a trace message using the underlying logger
-#[macro_export]
-#[collapse_debuginfo(yes)]
-macro_rules! trace {
-    ($s:literal $(, $x:expr)* $(,)?) => {
-        {
-            #[cfg(feature = "defmt")]
-            ::defmt::trace!($s $(, $x)*);
-            #[cfg(feature = "log")]
-            ::log::trace!($s $(, $x)*);
-            #[cfg(not(any(feature = "defmt", feature = "log")))]
-            let _ = ($( & $x ),*);
-        }
-    };
+#[cfg(all(feature = "log", feature = "defmt", not(doc)))]
+compile_error!("features `log` and `defmt` are mutually exclusive");
+
+#[cfg(all(not(doc), feature = "defmt"))]
+mod defmt {
+    /// Logs a trace message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! trace {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::defmt::trace!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs a debug message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::defmt::debug!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs an info message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! info {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::defmt::info!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs a warning using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! warn {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::defmt::warn!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs an error using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! error {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::defmt::error!($s $(, $x)*);
+            }
+        };
+    }
 }
 
-/// Logs a debug message using the underlying logger
-#[macro_export]
-#[collapse_debuginfo(yes)]
-macro_rules! debug {
-    ($s:literal $(, $x:expr)* $(,)?) => {
-        {
-            #[cfg(feature = "defmt")]
-            ::defmt::debug!($s $(, $x)*);
-            #[cfg(feature = "log")]
-            ::log::debug!($s $(, $x)*);
-            #[cfg(not(any(feature = "defmt", feature = "log")))]
-            let _ = ($( & $x ),*);
-        }
-    };
+#[cfg(all(not(doc), feature = "log"))]
+mod log {
+    /// Logs a trace message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! trace {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::log::trace!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs a debug message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::log::debug!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs an info message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! info {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::log::info!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs a warning using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! warn {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::log::warn!($s $(, $x)*);
+            }
+        };
+    }
+
+    /// Logs an error using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! error {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                ::log::error!($s $(, $x)*);
+            }
+        };
+    }
 }
 
-/// Logs a info message using the underlying logger
-#[macro_export]
-#[collapse_debuginfo(yes)]
-macro_rules! info {
-    ($s:literal $(, $x:expr)* $(,)?) => {
-        {
-            #[cfg(feature = "defmt")]
-            ::defmt::info!($s $(, $x)*);
-            #[cfg(feature = "log")]
-            ::log::info!($s $(, $x)*);
-            #[cfg(not(any(feature = "defmt", feature = "log")))]
-            let _ = ($( & $x ),*);
-        }
-    };
-}
+// Provide this implementation for `cargo doc`
+#[cfg(any(doc, not(any(feature = "defmt", feature = "log"))))]
+mod none {
+    /// Logs a trace message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! trace {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                let _ = ($( & $x ),*);
+            }
+        };
+    }
 
-/// Logs a warning using the underlying logger
-#[macro_export]
-#[collapse_debuginfo(yes)]
-macro_rules! warn {
-    ($s:literal $(, $x:expr)* $(,)?) => {
-        {
-            #[cfg(feature = "defmt")]
-            ::defmt::warn!($s $(, $x)*);
-            #[cfg(feature = "log")]
-            ::log::warn!($s $(, $x)*);
-            #[cfg(not(any(feature = "defmt", feature = "log")))]
-            let _ = ($( & $x ),*);
-        }
-    };
-}
+    /// Logs a debug message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! debug {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                let _ = ($( & $x ),*);
+            }
+        };
+    }
 
-/// Logs an error using the underlying logger
-#[macro_export]
-#[collapse_debuginfo(yes)]
-macro_rules! error {
-    ($s:literal $(, $x:expr)* $(,)?) => {
-        {
-            #[cfg(feature = "defmt")]
-            ::defmt::error!($s $(, $x)*);
-            #[cfg(feature = "log")]
-            ::log::error!($s $(, $x)*);
-            #[cfg(not(any(feature = "defmt", feature = "log")))]
-            let _ = ($( & $x ),*);
-        }
-    };
+    /// Logs an info message using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! info {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                let _ = ($( & $x ),*);
+            }
+        };
+    }
+
+    /// Logs a warning using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! warn {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                let _ = ($( & $x ),*);
+            }
+        };
+    }
+
+    /// Logs an error using the underlying logger
+    #[macro_export]
+    #[collapse_debuginfo(yes)]
+    macro_rules! error {
+        ($s:literal $(, $x:expr)* $(,)?) => {
+            {
+                let _ = ($( & $x ),*);
+            }
+        };
+    }
 }
