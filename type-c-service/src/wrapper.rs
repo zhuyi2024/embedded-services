60ac5ac0c6599561300df9738bcbdee7d2403e40
Robert Zieba <robertzieba@microsoft.com>
2025-02-27 12:36:55 -0700

    type-c-service/wrapper: Add logic to handle keeping track of port events

--- type-c-service/src/wrapper.rs
+++ type-c-service/src/wrapper.rs
@@ -1,7 +1,7 @@
 //! This module contains the `Controller` trait. Any types that implement this trait can be used with the `ControllerWrapper` struct
 //! which provides a bridge between various service messages and the actual controller functions.
 use core::array::from_fn;
-use core::cell::RefCell;
+use core::cell::{Cell, RefCell};
 
 use bitfield::BitMut;
 use embassy_futures::select::{select, select_array, Either};
@@ -20,6 +20,7 @@ pub struct ControllerWrapper<'a, const N: usize, C: Controller> {
     /// Power policy devices to interface with power policy service
     power: [policy::device::Device; N],
     controller: RefCell<C>,
+    active_events: [Cell<PortEventKind>; N],
 }
 
 impl<'a, const N: usize, C: Controller> ControllerWrapper<'a, N, C> {
@@ -29,6 +30,7 @@ impl<'a, const N: usize, C: Controller> ControllerWrapper<'a, N, C> {
             pd_controller,
             power,
             controller: RefCell::new(controller),
+            active_events: [const { Cell::new(PortEventKind::NONE) }; N],
         }
     }
 
@@ -153,6 +155,7 @@ impl<'a, const N: usize, C: Controller> ControllerWrapper<'a, N, C> {
             };
 
             if event == PortEventKind::NONE {
+                self.active_events[port].set(PortEventKind::NONE);
                 continue;
             }
 
@@ -183,6 +186,8 @@ impl<'a, const N: usize, C: Controller> ControllerWrapper<'a, N, C> {
             if event.new_power_contract_as_consumer() {
                 self.process_new_consumer_contract(power, &status).await;
             }
+
+            self.active_events[port].set(event);
         }
 
         self.pd_controller.notify_ports(port_events).await;
