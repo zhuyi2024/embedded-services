85e2e511712efa78a7ac86bf888c658d83796907
Robert Zieba <robertzieba@microsoft.com>
2025-03-07 16:21:52 -0700

    type-c-service: Fix implicit contract logic

--- type-c-service/src/driver/tps6699x.rs
+++ type-c-service/src/driver/tps6699x.rs
@@ -2,7 +2,7 @@ use core::array::from_fn;
 use core::iter::zip;
 
 use ::tps6699x::registers::field_sets::IntEventBus1;
-use ::tps6699x::registers::PlugMode;
+use ::tps6699x::registers::{PdCcPullUp, PlugMode};
 use ::tps6699x::{TPS66993_NUM_PORTS, TPS66994_NUM_PORTS};
 use embassy_sync::blocking_mutex::raw::RawMutex;
 use embedded_hal_async::i2c::I2c;
@@ -121,16 +121,20 @@ impl<'a, const N: usize, M: RawMutex, B: I2c> Controller for Tps6699x<'a, N, M, 
                     Contract::from(pdo)
                 });
             } else {
-                // Determine implicit/default contract
-                port_status.contract = Some(if pd_status.is_source() {
+                if pd_status.is_source() {
                     let current = TypecCurrent::try_from(port_control.typec_current()).map_err(Error::Pd)?;
                     debug!("Port{} type-C source current: {:#?}", port.0, current);
-                    Contract::Source(PowerCapability::from(current))
+                    port_status.contract = Some(Contract::Source(PowerCapability::from(current)))
                 } else {
-                    let current = TypecCurrent::try_from(pd_status.cc_pull_up()).map_err(Error::Pd)?;
-                    debug!("Port{} type-C sink current: {:#?}", port.0, current);
-                    Contract::Sink(PowerCapability::from(current))
-                });
+                    let pull = pd_status.cc_pull_up();
+                    if pull == PdCcPullUp::NoPull {
+                        port_status.contract = None
+                    } else {
+                        let current = TypecCurrent::try_from(pd_status.cc_pull_up()).map_err(Error::Pd)?;
+                        debug!("Port{} type-C sink current: {:#?}", port.0, current);
+                        port_status.contract = Some(Contract::Sink(PowerCapability::from(current)))
+                    }
+                }
             }
         }
 
