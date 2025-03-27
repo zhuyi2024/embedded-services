d903baef6bf248de44a4d3269a32a36fd716ccb1
Adam Sasine <adam.sasine@gmail.com>
2025-03-27 11:00:38 -0400

    Move UCSI module to embedded-usb-pd crate

--- embedded-service/src/type_c/mod.rs
+++ embedded-service/src/type_c/mod.rs
@@ -7,12 +7,6 @@ use crate::power::policy;
 
 pub mod controller;
 pub mod event;
-pub mod ucsi;
-
-/// Global port ID, used to unique identify a port
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "defmt", derive(defmt::Format))]
-pub struct GlobalPortId(pub u8);
 
 /// Controller ID
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
