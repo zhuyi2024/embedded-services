RobertZ2011/odp-embedded-services

embedded-service/type_c/comms: Create debug accessory connection message

embedded-service/src/type_c/comms.rs
@@ -1,0 +1,13 @@
+//! Comms service message definitions
+
+use super::GlobalPortId;
+
+/// Message generated when a debug acessory is connected or disconnected
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct DebugAccessoryMessage {
+    /// Port
+    pub port: GlobalPortId,
+    /// Connected
+    pub connected: bool,
+}

zhuyi2024/embedded-services

embedded-service/src/type_c/comms.rs
@@ -1,0 +1,13 @@
+//! Comms service message definitions
+
+use embedded_usb_pd::GlobalPortId;
+
+/// Message generated when a debug acessory is connected or disconnected
+#[derive(Copy, Clone, Debug)]
+#[cfg_attr(feature = "defmt", derive(defmt::Format))]
+pub struct DebugAccessoryMessage {
+    /// Port
+    pub port: GlobalPortId,
+    /// Connected
+    pub connected: bool,
+}